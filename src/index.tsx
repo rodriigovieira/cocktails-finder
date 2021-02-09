7146148576e643ce576c29034641d5cb43f1f74f
Rodrigo Vieira <rodriigovieira@gmail.com>
2021-02-09 01:52:42 -0300

    setting up react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,26 @@ import {createStackNavigator} from '@react-navigation/stack';
 import InitialScreen from './screens/InitialScreen';
 import {RootStackParamList} from './types';
 import SearchScreen from './screens/SearchScreen';
+import {Provider} from 'react-redux';
+import {store} from './redux';
 
 const RootStack = createStackNavigator<RootStackParamList>();
 
 function App() {
   return (
-    <NavigationContainer>
-      <RootStack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <RootStack.Screen name="InitialScreen" component={InitialScreen} />
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootStack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <RootStack.Screen name="InitialScreen" component={InitialScreen} />
 
-        <RootStack.Screen name="SearchScreen" component={SearchScreen} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+          <RootStack.Screen name="SearchScreen" component={SearchScreen} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
